a4642ad74384b74d3a8eaff13c32e2923bc3ca0a
Christian Kvalheim <christkv@gmail.com>
2014-03-21 11:12:27 +0100

    Fixed aws product dump and transform scripts

--- tools/aws_dump.js
+++ tools/aws_dump.js
@@ -24,12 +24,63 @@ var opHelper = new OperationHelper({
 // params: parameters for operation (optional)
 // callback(parsed, raw): callback function handling results. parsed = xml2js parsed response. raw = raw xml response
 // onError: function handling errors, otherwise all error messages are printed with console.log()
+var fetchNumberOfTotalPages = function(keyword, callback) {
+	opHelper.execute('ItemSearch', {
+	  'SearchIndex': 'Books',
+	  'Keywords': keyword,
+	  'Condition': 'All',
+	  'ResponseGroup': 'Images,Large'
+	}, function(object) { // you can add a second parameter here to examine the raw xml response
+	    callback(null, parseInt(object.ItemSearchResponse.Items[0].TotalPages[0], 10));
+	});	
+}
 
-opHelper.execute('ItemSearch', {
-  'SearchIndex': 'Books',
-  'Keywords': 'scifi',
-  'ResponseGroup': 'Images,Large'
-}, function(results) { // you can add a second parameter here to examine the raw xml response
-    // console.log(JSON.stringify(results, null, 2));
-    fs.writeFileSync(__dirname + "/../data.json", JSON.stringify(results, null, 2), 'utf8');
+// Fetch pages
+var fetchPage = function(keyword, index, totalPages, callback) {
+  // Are we done 
+  if(index == totalPages) {
+    return callback(null, null);
+  }
+
+  // Get the page
+	opHelper.execute('ItemSearch', {
+	  'SearchIndex': 'Books',
+	  'Keywords': keyword,
+	  'Condition': 'All',
+	  'ReviewsPage': "" + index,
+	  'ResponseGroup': 'Images,Large'
+	}, function(results) {
+			console.log("= FETCHED PAGE " + index + " for " + keyword);
+			// Write the data out to disk
+	    fs.writeFileSync(__dirname + "/../data/" + keyword + "_" + index + ".json", JSON.stringify(results, null, 2), 'utf8');	    
+	    // Fetch the next page
+	    setTimeout(function() {
+		    fetchPage(keyword, index + 1, totalPages, callback);
+	    }, 100);
+	});		
+}
+
+// Fetch by keywords
+var fetchByKeywords = function(keywords, callback) {
+	if(keywords.length == 0) {
+		return callback(null, null);
+	}
+	
+	// Get the keyword
+	var keyword = keywords.shift();
+	
+	// Fetch the total number of pages
+	fetchNumberOfTotalPages(keyword, function(err, totalPages) {
+		console.log("= START FETCHING");
+		totalPages = totalPages > 10 ? 10 : totalPages;
+
+		fetchPage(keyword, 0, totalPages, function() {
+			fetchByKeywords(keywords, callback)
+		});
+	});
+};
+
+fetchByKeywords(['scifi', 'history', 'cooking', 'programming'], function() {
+	console.log("= DONE");	
 });
+
--- tools/aws_transform.js
+++ tools/aws_transform.js
@@ -1,16 +1,8 @@
 var fs = require('fs'),
-	moment = require('moment');
+	moment = require('moment'),
+	MongoClient = require('mongodb').MongoClient;
 
-// Get raw data
-var data = fs.readFileSync(__dirname + "/../data.json", 'utf8');
-var object = JSON.parse(data);
-
-// Create our own object structure
-var objects = [];
-var parseToInt = ["NumberOfItems", "NumberOfPages"];
-var parseToDate = ["PublicationDate", "ReleaseDate"];
-var ignoreFields = ["EANList", "ItemDimensions", "Languages", "Creator"];
-
+//
 // Transform the images
 var convertImages = function(item, obj) {
 	var mapFunction = function(i) {
@@ -24,47 +16,85 @@ var convertImages = function(item, obj) {
 	}
 }
 
-// Iterate over all the items
-object.ItemSearchResponse.Items.forEach(function(item) {
+//
+// Convert the items form the data
+var convertItems = function(category, object, objects) {
+	var parseToInt = ["NumberOfItems", "NumberOfPages"];
+	var parseToDate = ["PublicationDate", "ReleaseDate"];
+	var ignoreFields = ["EANList", "ItemDimensions", "Languages", "Creator"];
 
-	item.Item.forEach(function(i) {
-		console.log("============== item")
-		console.dir(i)
+	object.ItemSearchResponse.Items.forEach(function(item) {
+		item.Item.forEach(function(i) {
+			var obj = {};
 
-		var obj = {};
+			// Convert all the images into a map
+			convertImages(i, obj);
 
-		// Convert all the images into a map
-		convertImages(i, obj);
-
-		// Do we have a EditorialReviews
-		if(Array.isArray(i.EditorialReviews) && i.EditorialReviews.length > 0) {
-			obj.description = i.EditorialReviews[0].EditorialReview[0].Content;
-		}
-
-		// Get all the attributes
-		i.ItemAttributes.forEach(function(attr) {
-			for(var name in attr) {
-				if(ignoreFields.indexOf(name) != -1) return;
-				// obj[name] = attr[name];
-				if(Array.isArray(attr[name]) && attr[name].length == 1) {
-					obj[name.toLowerCase()] = attr[name][0];
-				} else {
-					obj[name.toLowerCase()] = attr[name];
-				}
-
-				// If we have specific fields parse them
-				if(parseToInt.indexOf(name) != -1) {
-					obj[name.toLowerCase()] = parseInt(obj[name.toLowerCase()], 10);
-				}
-
-				if(parseToDate.indexOf(name) != -1) {
-					obj[name.toLowerCase()] = moment(obj[name.toLowerCase()]).toDate();
-				}
+			// Do we have a EditorialReviews
+			if(Array.isArray(i.EditorialReviews) && i.EditorialReviews.length > 0) {
+				obj.description = i.EditorialReviews[0].EditorialReview[0].Content;
 			}
-		});
 
-		objects.push(obj)
+			// Get all the attributes
+			i.ItemAttributes.forEach(function(attr) {
+				for(var name in attr) {
+					if(ignoreFields.indexOf(name) != -1) return;
+					// obj[name] = attr[name];
+					if(Array.isArray(attr[name]) && attr[name].length == 1) {
+						obj[name.toLowerCase()] = attr[name][0];
+					} else {
+						obj[name.toLowerCase()] = attr[name];
+					}
+
+					// If we have specific fields parse them
+					if(parseToInt.indexOf(name) != -1) {
+						obj[name.toLowerCase()] = parseInt(obj[name.toLowerCase()], 10);
+					}
+
+					if(parseToDate.indexOf(name) != -1) {
+						obj[name.toLowerCase()] = moment(obj[name.toLowerCase()]).toDate();
+					}
+				}
+			});
+
+			console.dir(i)
+
+			// Add category from keyword
+			obj.category = category;
+			obj.salesrank = i.SalesRank ? parseInt(i.SalesRank[0], 10) : 0;
+			// Save to finial object array
+			objects.push(obj)
+		});
+	});	
+}
+
+// Create our own object structure
+var objects = [];
+
+// Read all the files we need to parse
+var files = fs.readdirSync(__dirname + "/../data");
+files.forEach(function(file) {
+	var category = file.split(/_/)[0];
+	// Read raw data
+	var data = fs.readFileSync(__dirname + "/../data/" + file, 'utf8');
+	// Parse to JSON
+	var object = JSON.parse(data);
+	// Translate
+	convertItems(category, object, objects);
+});
+
+// Connect to MongoDB and insert into products table
+MongoClient.connect("mongodb://localhost:27017/ecommerce", function(err, db) {
+	if(err) throw err;
+	db.collection('products').drop(function(err) {
+		// if(err) throw err;
+	
+		db.collection('products').insert(objects, function(err) {
+			if(err) throw err;
+			db.close();
+			process.exit(0);
+		});
 	});
 });
 
-console.log(JSON.stringify(objects, null, 2))
+// console.log(JSON.stringify(objects, null, 2))
