92972d36bdedb73512af0a11ad6be8c837311a50
Christian Kvalheim <christkv@gmail.com>
2014-05-03 10:05:02 +0200

    Updated app.js

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ if('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', routes.index);
+app.get('/', rout)
 
 // Start http server
 http.createServer(app).listen(app.get('port'), function(){
